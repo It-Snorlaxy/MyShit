68ce6831da346f5712690e9091f9dd7007d8a3cb
Anonymous <oliverlehman2003@gmail.com>
2022-05-06 16:02:28 +0200

    idk why I try

--- GAme thing/Tutorial/KidsCanCode/main.py
+++ GAme thing/Tutorial/KidsCanCode/main.py
@@ -1,41 +1,57 @@
-# Pygame template - skeleton for new games made by KidsCanCode
+# pygame template - skeleton for new games made by KidsCanCode
 
-import pygame
+import pygame as pg
 import random
+#from sprites import *
 
-width = 360
-height = 480
-fps = 30
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
 
 # Define colors
-White = (255, 255, 255)
-Black = (0, 0, 0)
-Red = (255, 0, 0)
-Green = (0, 255, 0)
-Blue = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+class Player(pg.sprite.Sprite):
+    # sprite for the Player
+    def __init__(self):
+        #Needed for every sprite
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load("player.png")
+        self.rect = self.image.get.rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
 
 #initialize pygame and create window
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
+pg.init()
+pg.mixer.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("My Game")
+clock = pg.time.Clock()
 
+all_sprites = pg.sprite.Group()
+player = Player()
+all_sprites.add(player)
 #Game loop
 running = True
 while running:
     #keep loop running at the right speed
-    clock.tick(fps)
+    clock.tick(FPS)
     # v There are 3 componets of a game loop v
     # Process input (events)
-    for event in pygame.event.get():
+    for event in pg.event.get():
         # check for closing the window
-        if event.type == pygame.QUIT:
+        if event.type == pg.QUIT:
             running = False
     # Update
-    # Draw/Render/Display
-    screen.fill(Black)
-    # *after* drawing everything, flip the display -Double Buffering-
-    pygame.display.flip()
+    all_sprites.update()
 
-pygame.quit()
+    # Draw/Render/Display
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # *after* drawing everything, flip the display -Double Buffering-
+    pg.display.flip()
+
+pg.quit()
--- GAme thing/Tutorial/KidsCanCode/sprites.py
+++ GAme thing/Tutorial/KidsCanCode/sprites.py
@@ -1,34 +1,14 @@
-import pygame as pg
-from settings import *
+import pygame
+import random
+from main import *
 
-class Player(pg.sprite.Sprite):
-    def __init__(self, game, x, y):
-        self.groups = game.all_sprites
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(YELLOW)
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
+player = Player()
 
-    def move(self, dx=0, dy=0):
-        self.x += dx
-        self.y += dy
-
-    def update(self):
-        self.rect.x = self.x * TILESIZE
-        self.rect.y = self.y * TILESIZE
-
-class Wall(pg.sprite.Sprite):
-    def __init__(self, game, x, y):
-        self.groups = game.all_sprites, game.walls
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(GREEN)
-        self.rect = self.image.get_rect()
-        self.x = x
-        self.y = y
-        self.rect.x = x * TILESIZE
-        self.rect.y = y * TILESIZE
+class Player(pygame.sprite.Sprite):
+    # sprite for the Player
+    def __init__(self):
+        #Needed for every sprite
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("player.png")
+        self.rect = self.image.get.rect()
+        self.rect.center = (width / 2, height / 2)
