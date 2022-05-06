93d6ae6e43d2a1907e02c6b2837ec19650ee10d2
Anonymous <oliverlehman2003@gmail.com>
2022-05-06 19:49:05 +0200

    Everything works

--- GAme thing/TileBased_Game/tilemap.py
+++ GAme thing/TileBased_Game/tilemap.py
@@ -6,7 +6,7 @@ class Map:
         self.data = []
         with open(filename, 'rt') as f:
             for line in f:
-                self.data.append(line)
+                self.data.append(line.strip())
 
         self.tilewidth = len(self.data[0])
         self.tileheight = len(self.data)
@@ -14,7 +14,7 @@ class Map:
         self.height = self.tileheight * TILESIZE
 
 class Camera:
-    def __init(self, width, height):
+    def __init__(self, width, height):
         self.camera = pg.Rect(0, 0, width, height)
         self.width = width
         self.height = height
