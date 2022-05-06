ed2322c26cd18a68c6808893bba1461598cf7a61
Anonymous <oliverlehman2003@gmail.com>
2022-05-06 16:27:36 +0200

    Final touch - The template is functional

--- GAme thing/Tutorial/PygameTemplate/main.py
+++ GAme thing/Tutorial/PygameTemplate/main.py
@@ -2,10 +2,12 @@
 
 import pygame as pg
 import random
+import keyboard
+import os
 #from sprites import *
 
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 800
+HEIGHT = 600
 FPS = 30
 
 # Define colors
@@ -15,14 +17,35 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+# set up assets folder
+#
+game_folder = os.path.dirname(__file__)
+Assets_folder = os.path.join(game_folder, "Assets")
+
+
+
 class Player(pg.sprite.Sprite):
     # sprite for the Player
     def __init__(self):
         #Needed for every sprite
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.image.load("player.png")
+        self.image = pg.image.load(os.path.join(Assets_folder, "player.png")).convert()
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+    def update(self):
+        if keyboard.is_pressed('d'):
+            self.rect.x += 5 # move it to the right
+        if keyboard.is_pressed('s'):
+            self.rect.y += 5 # move it down
+        if keyboard.is_pressed('a'):
+            self.rect.x -= 5 # move it to the left
+        if keyboard.is_pressed('w'):
+            self.rect.y -= 5 # move it up
+
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
 
 #initialize pygame and create window
 pg.init()
@@ -49,7 +72,7 @@ while running:
     all_sprites.update()
 
     # Draw/Render/Display
-    screen.fill(BLACK)
+    screen.fill(BLUE)
     all_sprites.draw(screen)
     # *after* drawing everything, flip the display -Double Buffering-
     pg.display.flip()
