a945c9bc557fc3190f507e603faa6fddec26908e
Anonymous <oliverlehman2003@gmail.com>
2022-05-10 13:46:59 +0200

    code

---
+++ DDU - EKSAMEN/main.py
@@ -0,0 +1,33 @@
+import bcrypt
+import hashlib
+LR = None
+
+salt = bcrypt.gensalt()
+
+def register ():
+    print('Making a user')
+    user = input('Navn: ')
+    hashed = bcrypt.hashpw(input('password: ').encode('utf-8'), salt)
+    file = open('user_details.txt','a')
+    file.write(user + ',' + str(hashed) + ',' + str(salt) + '\n')
+    file.close()
+    logreg()
+
+def login (logname, logpass):
+    file = open('user_details.txt','r')
+    for i in file:
+        user, hashed, salt = i.split(',')
+    bcrypt.checkpw(logpass, hashed)
+    file.close()
+
+def logreg():
+    loginregist = input('log/reg? ')
+    if loginregist == 'log':
+        print('Du er ved at logge ind')
+        logname = input('Navn: ')
+        logpass = bcrypt.hashpw(input('password: ').encode('utf-8'), salt)
+        login(logname, logpass)
+    elif loginregist == 'reg':
+        register()
+    else:
+        logreg()
